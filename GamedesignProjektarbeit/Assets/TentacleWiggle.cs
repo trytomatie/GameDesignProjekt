be02b70320aff1c03d5bbf2657479a86d25ac829
trytomatie <trytomatie@gmail.com>
2021-12-21 14:36:59 +0100

    First Commit
    IK test

---
+++ GamedesignProjektarbeit/Assets/TentacleWiggle.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TentacleWiggle : MonoBehaviour
+{
+
+    public List<Transform> tentacles;
+    public float wiggleAmount = 0.05f;
+    public float wigleScale = 1;
+    private List<int> randomOffset = new List<int>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < tentacles.Count;i++)
+        {
+            randomOffset.Add(Random.Range(1, 100000));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int i = 0;
+        foreach (Transform tentacle in tentacles)
+        {
+            tentacle.Rotate(0,0,Mathf.Sin((Time.time * wigleScale + randomOffset[i])) * wiggleAmount);
+            i++;
+        }
+
+    }
+}
