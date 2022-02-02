184e1a8ea390f8b3498d0e5c20d016f1066e704b
MarkusSchwalb <96125033+MarkusSchwalb@users.noreply.github.com>
2022-02-02 09:46:34 +0100

    Enemy Logic patch1
    ich habe im script enemy das Enemy Death event ein wenig verändert um die Death animation abzuspielen zudem hat gegner 1 nun auch eine Get damage Animation. im allgemeinen hat der Enemy alle Animationen eingebunden bekommen. Ich habe im Enemy Script auch ein get Damage event angelegt aber bis jetzt spielt es einfach nur die Damage Animation ab.

--- GamedesignProjektarbeit/Assets/Scripts/Enemy.cs
+++ GamedesignProjektarbeit/Assets/Scripts/Enemy.cs
@@ -5,25 +5,48 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
 
+
+    public AnimationClip dieAni;
+
+    private Animator enemyAnimator;
+    private Vector2 currentPos;
+    private Vector2 lastPos;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyAnimator = GetComponent<Animator>();       //get Animator Component
+        currentPos = new Vector2(transform.position.x, transform.position.y);   //set both Positions so there are not null
+        lastPos = new Vector2(transform.position.x, transform.position.y);      
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        currentPos = new Vector2(transform.position.x, transform.position.y);           //set current Position
+        float walkingDistance = Mathf.Abs(Vector2.Distance(currentPos, lastPos));       //measure the distance between current and last position in order to get a movenent speed
+
+        enemyAnimator.SetFloat("movementSpeed", walkingDistance);                       //change movement speed im animator so it plays walking animation
+
+        lastPos = currentPos;                                                           //set last position to current position for next update
     }
+
+    public void DamageEvent()
+    {
+        enemyAnimator.SetTrigger("getDamage");                                          //if event is triggered play animation
+    }
+
 
     public void EnemyDeath()
     {
-        Destroy(gameObject);
+        enemyAnimator.SetBool("isAlive",false);         //play Death Animation (Markus)
+        Destroy(gameObject, dieAni.length);             //ergaenzung der eines Sterbezeitraums damit die sterbeanimation ausgefuehrt werden kann (Markus)
+
         SpawnManager.instance.enemyCount = SpawnManager.instance.enemyCount - 1;
 
         GetComponent<Collider2D>().enabled = false;
         GetComponent<FollowPath>().enabled = false;
-
+        
     }
 }
